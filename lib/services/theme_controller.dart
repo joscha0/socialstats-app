57332270cac462c5b12235de412f6e2db1054362
Joscha von Andrian <joschavonandrian@gmail.com>
2021-12-29 16:02:39 +0100

    customize theme

--- lib/services/theme_controller.dart
+++ lib/services/theme_controller.dart
@@ -3,6 +3,20 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ThemeController extends GetxController {
-  Rx<ThemeData> theme =
-      ThemeData.light().copyWith(textTheme: GoogleFonts.bangersTextTheme()).obs;
+  Rx<ThemeData> theme = ThemeData.light()
+      .copyWith(
+        textTheme: GoogleFonts.bangersTextTheme(),
+        splashColor: Colors.transparent,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.black,
+          secondary: Colors.black,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+          ),
+        ),
+      )
+      .obs;
 }
