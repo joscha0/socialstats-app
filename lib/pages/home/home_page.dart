913bceff4919c43218a629f61c828940f0b5f614
Joscha von Andrian <joschavonandrian@gmail.com>
2021-12-29 16:02:05 +0100

    add bottom app bar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:igstats/pages/profile/profile_page.dart';
+import 'package:igstats/pages/search/search_page.dart';
 
 import 'home_controller.dart';
 
@@ -8,19 +10,38 @@ class HomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Search',
+    return GetX<HomeController>(
+        init: HomeController(),
+        builder: (c) {
+          return Scaffold(
+            body: IndexedStack(
+              index: c.tabIndex.value,
+              children: [
+                SearchPage(),
+                ProfilePage(),
+              ],
             ),
-          ),
-        ),
-      ],
-    ));
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: (value) => c.tabIndex.value = value,
+              currentIndex: c.tabIndex.value,
+              backgroundColor: Colors.black,
+              selectedItemColor: Colors.white,
+              unselectedItemColor: Colors.white70,
+              selectedIconTheme: const IconThemeData(opacity: 0.0, size: 0),
+              unselectedIconTheme: const IconThemeData(opacity: 0.0, size: 0),
+              selectedFontSize: 24,
+              unselectedFontSize: 22,
+              items: [
+                _bottomNavigationBarItem('search'),
+                _bottomNavigationBarItem('profile')
+              ],
+            ),
+          );
+        });
+  }
+
+  BottomNavigationBarItem _bottomNavigationBarItem(String label) {
+    return BottomNavigationBarItem(
+        icon: const Icon(Icons.ac_unit), label: label);
   }
 }
