5f948c551e3627d711acbe07003eeda783da7ef5
Joscha von Andrian <joschavonandrian@gmail.com>
2021-12-29 20:59:00 +0100

    add always keep alive

--- lib/pages/home/home_controller.dart
+++ lib/pages/home/home_controller.dart
@@ -1,5 +1,13 @@
 import 'package:get/get.dart';
+import 'package:igstats/pages/profile/profile_controller.dart';
 
 class HomeController extends GetxController {
   RxInt tabIndex = 0.obs;
+
+  switchTab(int value) {
+    tabIndex.value = value;
+    // if (value != 1) {
+    //   Get.find<ProfileController>().closeVideoControllers();
+    // }
+  }
 }
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -16,13 +16,13 @@ class HomePage extends GetView<HomeController> {
           return Scaffold(
             body: IndexedStack(
               index: c.tabIndex.value,
-              children: [
-                SearchPage(),
-                ProfilePage(),
+              children: const [
+                SearchTab(),
+                ProfileTab(),
               ],
             ),
             bottomNavigationBar: BottomNavigationBar(
-              onTap: (value) => c.tabIndex.value = value,
+              onTap: (value) => c.switchTab(value),
               currentIndex: c.tabIndex.value,
               backgroundColor: Colors.black,
               selectedItemColor: Colors.white,
@@ -44,4 +44,44 @@ class HomePage extends GetView<HomeController> {
     return BottomNavigationBarItem(
         icon: const Icon(Icons.ac_unit), label: label);
   }
+}
+
+class ProfileTab extends StatefulWidget {
+  const ProfileTab({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    return const ProfilePage();
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
+
+class SearchTab extends StatefulWidget {
+  const SearchTab({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SearchTab> createState() => _SearchTabState();
+}
+
+class _SearchTabState extends State<SearchTab>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    return const SearchPage();
+  }
+
+  @override
+  bool get wantKeepAlive => true;
 }
